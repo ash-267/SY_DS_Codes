95d2359f73129066a31a0146c1403cfee41ca31c
ash-267 <ayushsadavarte2024.comp@mmcoe.edu.in>
2025-09-10 08:50:55 +0530

    Update 040Task_Scheduler.cpp

--- 040Task_Scheduler.cpp
+++ 040Task_Scheduler.cpp
@@ -43,6 +43,7 @@ public:
             insert_node(newnode); 
             cout << "\nDo you want to add more tasks? (y/n): ";
             cin >> ch;
+            cout << endl;
         } while (ch == 'y' || ch == 'Y');
     }
 
@@ -81,9 +82,10 @@ public:
         cout << "\nScheduled Tasks:\n";
         Node* temp = head;
         while (temp != NULL) {
-            cout << "\nTask: " << temp->task_name << " | \tPriority: " << temp->priority << " | \tExecution Time: " << temp->exe_time << "s\n" << endl;;
+            cout << "Task: " << temp->task_name << " | \tPriority: " << temp->priority << " | \tExecution Time: " << temp->exe_time << "s" << endl;;
             temp = temp->next;
         }
+        cout << endl;
     }
 
     void delete_node() {
@@ -92,12 +94,12 @@ public:
         }
         cout << "\nExecuting Tasks:" << endl;
         while (head != NULL) {
-            cout << "Executing '" << head->task_name << "' (Time: " << head->exe_time << "s)\n";
+            cout << "Task: " << head->task_name << " , Time: " << head->exe_time << "s, Priority: " << head->priority << " is scheduled for execution." << endl;
             Node* temp = head;
             head = head->next;
             delete temp;
         }
-        cout << "All tasks executed!\n";
+        cout << "All tasks executed!\n" << endl;
     }
 };
 
@@ -106,9 +108,9 @@ int main() {
     int choice;
     
     do {
-        cout << "\n===== Simple Task Scheduler =====" << endl;
+        cout << "Simple Task Scheduler:" << endl;
         cout << "1. Add Tasks\n2. Display Tasks\n3. Execute All Tasks\n4. Exit" << endl;
-        cout << "Enter your choice: ";
+        cout << "Choice: ";
         cin >> choice;
         switch (choice) {
             case 1:
@@ -121,11 +123,135 @@ int main() {
                 scheduler.delete_node();
                 break;
             case 4:
-                cout << "Thankyou for using task scheduler\n";
+                cout << "Thank you for using task scheduler\n";
                 break;
             default:
                 cout << "Invalid choice! Try again.\n";
         }
     } while (choice != 4);
 return 0;
-}
+}#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+class Node {
+public:
+    string task_name;
+    int priority;
+    int exe_time;
+    Node* next;
+
+    Node(string tn, int p, int e) {
+        task_name = tn;
+        priority = p;
+        exe_time = e;
+        next = NULL;
+    }
+};
+
+class TaskScheduler {
+private:
+    Node* head;
+public:
+    TaskScheduler() {
+        head = NULL;
+    }
+
+    
+    void create() {
+        char ch;
+        do {
+            Node* newnode = getnode(); 
+            insert_node(newnode); 
+            cout << "\nDo you want to add more tasks? (y/n): ";
+            cin >> ch;
+            cout << endl;
+        } while (ch == 'y' || ch == 'Y');
+    }
+
+
+    Node* getnode() {
+        string tn;
+        int p, e;
+        cout << "\nEnter task name: ";
+        cin >> tn;
+        cout << "Enter priority: ";
+        cin >> p;
+        cout << "Enter execution time: ";
+        cin >> e;
+        Node* newnode = new Node(tn, p, e);
+        return newnode;
+    }
+
+    void insert_node(Node* newnode) {
+        if (head == NULL || newnode->priority > head->priority) {
+            newnode->next = head;
+            head = newnode;
+        } else {
+            Node* temp = head;
+            while (temp->next != NULL && temp->next->priority >= newnode->priority) {
+                temp = temp->next;
+            }
+            newnode->next = temp->next;
+            temp->next = newnode;
+        }
+    }
+
+    void display() {
+        if (head == NULL) {
+            cout << "\nNo tasks scheduled!\n";
+        }
+        cout << "\nScheduled Tasks:\n";
+        Node* temp = head;
+        while (temp != NULL) {
+            cout << "Task: " << temp->task_name << " | \tPriority: " << temp->priority << " | \tExecution Time: " << temp->exe_time << "s" << endl;;
+            temp = temp->next;
+        }
+        cout << endl;
+    }
+
+    void delete_node() {
+        if (head == NULL) {
+        cout << "No tasks to execute!" << endl;
+        }
+        cout << "\nExecuting Tasks:" << endl;
+        while (head != NULL) {
+            cout << "Task: " << head->task_name << " , Time: " << head->exe_time << "s, Priority: " << head->priority << " is scheduled for execution." << endl;
+            Node* temp = head;
+            head = head->next;
+            delete temp;
+        }
+        cout << "All tasks executed!\n" << endl;
+    }
+};
+
+int main() {
+    TaskScheduler scheduler;
+    int choice;
+    
+    do {
+        cout << "Simple Task Scheduler:" << endl;
+        cout << "1. Add Tasks\n2. Display Tasks\n3. Execute All Tasks\n4. Exit" << endl;
+        cout << "Choice: ";
+        cin >> choice;
+        switch (choice) {
+            case 1:
+                scheduler.create();
+                break;
+            case 2:
+                scheduler.display();
+                break;
+            case 3:
+                scheduler.delete_node();
+                break;
+            case 4:
+                cout << "Thank you for using task scheduler\n";
+                break;
+            default:
+                cout << "Invalid choice! Try again.\n";
+        }
+    } while (choice != 4);
+return 0;
+} 
